2e9b867a300f8ed187ff3b80d401e112dfa8a10c
LaberryO <laberryo0912@gmail.com>
2025-03-26 15:41:42 +0900

    refactor: 리스케일러 적용 및 효율성 강화

--- Resource/Entity/MobFly.py
+++ Resource/Entity/MobFly.py
@@ -1,24 +1,26 @@
 from ..Data.Screen import Screen;
-from ..System.PathLoader import ImageLoader;
-from .Monster import Monster;
+from ..System.PathLoader import imageLoader;
+from ..System.ImageEditor import rescale;
+from .Entity import Entity;
+
 import random, pygame;
 
 # 파리 (몬스터)
-class MobFly(Monster):
+class MobFly(Entity):
     # 초기값 설정
     def __init__(self):
-        self.x = random.randint(0, Screen().getWidth()-32);
+        self.x = random.randint(0, Screen().getWidth()-64);
         self.y = -100;
         self.dy = 0.1;
 
         # Image
         self.image = [
-            pygame.image.load(ImageLoader("mobFly_0.png")),
-            pygame.image.load(ImageLoader("mobFly_1.png"))
+            rescale(imageLoader("mobFly_0.png"), 64),
+            rescale(imageLoader("mobFly_1.png"), 64)
         ];
     def move(self, deltaTime):
-        self.dy += 0.15 * deltaTime;
-        self.y += self.dy;
+        self.dy += 0.15;
+        self.y += self.dy * deltaTime;
     def draw(self, screen):
         screen.blit(self.image[0], (self.x, self.y));
     def offScreen(self):
