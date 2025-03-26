a99a8f7e065637228114cca0b80e83feb31b72bc
LaberryO <laberryo0912@gmail.com>
2025-03-26 18:40:22 +0900

    feat: MobFly 애니메이션 추가

--- Resource/Entity/MobFly.py
+++ Resource/Entity/MobFly.py
@@ -9,20 +9,36 @@ import random, pygame;
 class MobFly(Entity):
     # 초기값 설정
     def __init__(self):
+        self.size = 64;
         self.x = random.randint(0, Screen().getWidth() - self.size);
         self.y = -100;
         self.dy = 0.1;
-        self.size = 64;
+        self.accSpeed = 0.15;
 
         # Image
         self.image = [
             rescale(imageLoader("mobFly_0.png"), self.size),
             rescale(imageLoader("mobFly_1.png"), self.size)
         ];
+    
+        # 애니메이션 변수
+        self.currentFrame = 0;
+        self.frameTime = 0.0; # 누적 시간 저장
+        self.frameInterval = 0.25; # 초 기준
+    
     def move(self, deltaTime):
-        self.dy += 0.15;
+        self.dy += self.accSpeed;
         self.y += self.dy * deltaTime;
+    
+        self.frameTime += deltaTime;
+        if self.frameTime >= self.frameInterval:
+            self.frameTime = 0;
+            # 애니메이션 확장성을 위한 구문
+            # 일종의 시계처럼 계속 루프함.
+            self.currentFrame = (self.currentFrame + 1) % len(self.image);
+    
     def draw(self, screen):
-        screen.blit(self.image[0], (self.x, self.y));
+        screen.blit(self.image[self.currentFrame], (self.x, self.y));
+    
     def offScreen(self):
         return self.y > Screen().getHeight();
